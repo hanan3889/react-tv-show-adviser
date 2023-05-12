57d99fd2084c041a09d0525e0ec3dcbd27577e0d
hanan3889 <rebaia_hanan@yahoo.fr>
2023-05-12 12:02:18 +0200

    add new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,55 @@
+import { useEffect, useState } from "react";
 import { TVShowAPI } from "./api/tv-show";
+import { TVShowDetail } from "./components/TVShowDetail/TVShowDetail";
+import { Logo } from "./components/Logo/Logo";
 import "./global.css";
 import s from "./style.module.css";
-
-TVShowAPI.fetchPopulars();
+import { BACKDROP_BASE_URL } from "./config";
+import logo from "./assets/images/logo.png";
+import { TVShowListItem } from "./components/TVShowListItem/TVShowListItem";
 export function App() {
+    const [currentTVShow, setcurrentTVShow] = useState();
+
+    async function fetchPopulars() {
+        const populars = await TVShowAPI.fetchPopulars();
+        if (populars.length > 0) {
+            setcurrentTVShow(populars[0]);
+        }
+    }
+
+    useEffect(() => {
+        fetchPopulars();
+    }, []);
+
     return (
-        <div className={s.main_container}>
+        <div
+            className={s.main_container}
+            style={{
+                background: currentTVShow
+                    ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+                    : "black",
+            }}
+        >
             <div className={s.header}>
                 <div className="row">
                     <div className="col-4">
-                        <div>Logo here</div>
-                        <div>subtitle</div>
+                        <Logo
+                            image={logo}
+                            title=""
+                            subtitle="Find a show you may like"
+                        />
                     </div>
                     <div className="col-md-12 col-lg-4">
-                        <input type="text" />
+                        <input style={{ width: "100%" }} type="text" />
                     </div>
                 </div>
             </div>
-            <div className={s.tv_show_details}>Tv show detail</div>
-            <div className={s.recommendations}>Recommended tv shows</div>
+            <div className={s.tv_show_details}>
+                {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
+            </div>
+            <div className={s.recommendations}>
+                {currentTVShow && <TVShowListItem tvShow={currentTVShow} />}
+            </div>
         </div>
     );
 }
